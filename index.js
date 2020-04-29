61ac6aff00c04622435811a488ad2ef455bc5b21
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-04-29 16:08:32 -0400

    making progress

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const startGameButton = document.querySelector('.start-button');
-const gameAlert = document.querySelector('.game-alert');
 const resetButton = document.querySelector('.reset-button');
 const green = document.querySelector('.green-block');
 const red = document.querySelector('.red-block');
 const yellow = document.querySelector('.yellow-block');
 const blue = document.querySelector('.blue-block');
+let randomBlocks = ['green', 'red', 'yellow', 'blue'];
+let gameAlert = document.querySelector('.game-alert');
 let gameSequence = [];
 let playerSequence = [];
+let level = 0;
 
 // ===============================================
 // START GAME
@@ -15,18 +17,10 @@ let playerSequence = [];
 startGameButton.addEventListener('click', startGame);
 
 function startGame() {
-  // START A NEW ARRAY WITH FIRST RANDOM BLOCK
-  let randomBlocks = ['green', 'red', 'yellow', 'blue'];
-  let random = Math.floor(Math.random() * 4);
-  let firstBlock = randomBlocks[random];
-
-  // PUSH RANDOM BLOCK ONLY IF GAME HASN'T STARTED
-  if (gameSequence.length < 1) {
-    gameSequence.push(firstBlock);
-    gameAlert.innerHTML = 'LEVEL 1';
-  }
   // IF PLAYER HITS START AGAIN ALERT THEM THAT THE GAME HAS STARTED
-  else if (gameSequence.length >= 1) {
+  if (gameSequence.length < 1) {
+    nextSequence();
+  } else {
     gameAlert.innerHTML = 'GAME HAS STARTED ALREADY';
   }
 
@@ -43,6 +37,22 @@ function startGame() {
 }
 
 // ===============================================
+// NEXT SEQUENCE
+// ===============================================
+function nextSequence() {
+  // START A NEW ARRAY WITH FIRST RANDOM BLOCK
+  let random = Math.floor(Math.random() * 4);
+  let randomBlock = randomBlocks[random];
+
+  // PUSH NEXT RANDOM BLOCK INTO (gameSequence)
+  gameSequence.push(randomBlock);
+  level++;
+  gameAlert.innerHTML = `LEVEL ${level}`;
+
+  console.log(`gamesequence: ${gameSequence}`);
+}
+
+// ===============================================
 // RESET GAME
 // ===============================================
 
@@ -52,6 +62,8 @@ function resetGame() {
   // CLEAR THE ARRAYS (gameSequence) and (playerSequence)
   gameSequence = [];
   playerSequence = [];
+  level = 0;
+
   // INITIATE START
   startGame();
 }
@@ -60,31 +72,58 @@ function resetGame() {
 // GAME SEQUENCE AND PLAYER SEQUENCE
 // ===============================================
 
+// GREEN BLOCK CLICK
 green.addEventListener('click', () => {
   playerSequence.push('green');
 
-  if (gameSequence !== playerSequence) {
+  if (gameSequence[level - 1] !== playerSequence[level - 1]) {
     gameAlert.innerHTML = 'GAME OVER';
+  } else {
+    nextSequence();
+    level++;
+    gameAlert = `LEVEL ${level}`;
+    console.log(`playersequence: ${playerSequence}`);
   }
 });
+
+// RED BLOCK CLICK
 red.addEventListener('click', () => {
   playerSequence.push('red');
 
-  if (gameSequence !== playerSequence) {
+  if (gameSequence[level - 1] !== playerSequence[level - 1]) {
     gameAlert.innerHTML = 'GAME OVER';
+  } else {
+    nextSequence();
+    level++;
+    gameAlert = `LEVEL ${level}`;
+    console.log(`playersequence: ${playerSequence}`);
   }
 });
+
+// YELLOW BLOCK CLICK
 yellow.addEventListener('click', () => {
   playerSequence.push('yellow');
 
-  if (gameSequence !== playerSequence) {
+  if (gameSequence[level - 1] !== playerSequence[level - 1]) {
     gameAlert.innerHTML = 'GAME OVER';
+  } else {
+    nextSequence();
+    level++;
+    gameAlert = `LEVEL ${level}`;
+    console.log(`playersequence: ${playerSequence}`);
   }
 });
+
+// BLUE BLOCK CLICK
 blue.addEventListener('click', () => {
   playerSequence.push('blue');
 
-  if (gameSequence !== playerSequence) {
+  if (gameSequence[level - 1] !== playerSequence[level - 1]) {
     gameAlert.innerHTML = 'GAME OVER';
+  } else {
+    nextSequence();
+    level++;
+    gameAlert = `LEVEL ${level}`;
+    console.log(`playersequence: ${playerSequence}`);
   }
 });
