e2b38f3824f10d549f85086d320420626ab0e015
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-04-29 16:31:22 -0400

    final function to compare player vs game needed

--- index.js
+++ index.js
@@ -24,14 +24,18 @@ function startGame() {
     gameAlert.innerHTML = 'GAME HAS STARTED ALREADY';
   }
 
+  flashBlocks();
+}
+
+function flashBlocks() {
   // FLASH THE FIRST COLOR BLOCK
-  if (gameSequence[0] === 'green') {
+  if (gameSequence[level - 1] === 'green') {
     green.classList.add('flashit');
-  } else if (gameSequence[0] === 'red') {
+  } else if (gameSequence[level - 1] === 'red') {
     red.classList.add('flashit');
-  } else if (gameSequence[0] === 'yellow') {
+  } else if (gameSequence[level - 1] === 'yellow') {
     yellow.classList.add('flashit');
-  } else if (gameSequence[0] === 'blue') {
+  } else if (gameSequence[level - 1] === 'blue') {
     blue.classList.add('flashit');
   }
 }
@@ -40,15 +44,27 @@ function startGame() {
 // NEXT SEQUENCE
 // ===============================================
 function nextSequence() {
+  // RESET PLAYERSEQUENCE
+  // playerSequence = [];
+
+  // REMOVE FLASH CLASSES FOR REPEAT
+  green.classList.remove('flashit');
+  red.classList.remove('flashit');
+  yellow.classList.remove('flashit');
+  blue.classList.remove('flashit');
+
   // START A NEW ARRAY WITH FIRST RANDOM BLOCK
   let random = Math.floor(Math.random() * 4);
   let randomBlock = randomBlocks[random];
 
   // PUSH NEXT RANDOM BLOCK INTO (gameSequence)
   gameSequence.push(randomBlock);
+
   level++;
   gameAlert.innerHTML = `LEVEL ${level}`;
+  flashBlocks();
 
+  console.log(`playersequence: ${playerSequence}`);
   console.log(`gamesequence: ${gameSequence}`);
 }
 
@@ -80,9 +96,6 @@ green.addEventListener('click', () => {
     gameAlert.innerHTML = 'GAME OVER';
   } else {
     nextSequence();
-    level++;
-    gameAlert = `LEVEL ${level}`;
-    console.log(`playersequence: ${playerSequence}`);
   }
 });
 
@@ -94,9 +107,6 @@ red.addEventListener('click', () => {
     gameAlert.innerHTML = 'GAME OVER';
   } else {
     nextSequence();
-    level++;
-    gameAlert = `LEVEL ${level}`;
-    console.log(`playersequence: ${playerSequence}`);
   }
 });
 
@@ -108,9 +118,6 @@ yellow.addEventListener('click', () => {
     gameAlert.innerHTML = 'GAME OVER';
   } else {
     nextSequence();
-    level++;
-    gameAlert = `LEVEL ${level}`;
-    console.log(`playersequence: ${playerSequence}`);
   }
 });
 
@@ -122,8 +129,5 @@ blue.addEventListener('click', () => {
     gameAlert.innerHTML = 'GAME OVER';
   } else {
     nextSequence();
-    level++;
-    gameAlert = `LEVEL ${level}`;
-    console.log(`playersequence: ${playerSequence}`);
   }
 });
